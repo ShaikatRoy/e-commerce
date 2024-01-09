cbfcf855d049d7e46a710c94ac6204fd6684e047
Shaikat roy <shaikatroy258@gmail.com>
2024-01-09 23:42:35 +0600

    radio checkbox added

--- src/app/(pages)/products/Filters/index.tsx
+++ src/app/(pages)/products/Filters/index.tsx
@@ -1,7 +1,47 @@
+'use client'
+
 import React from 'react'
 
-const Filters = () => {
-  return <div>Filters</div>
+import { Category } from '../../../../payload/payload-types'
+import Categories from '../../../_components/Categories/index'
+import { Checkbox } from '../../../_components/Checkbox/Index'
+import { HR } from '../../../_components/HR/index'
+import { useFilter } from '../../../_providers/Filter'
+
+import classes from './index.module.scss'
+
+const Filters = ({ categories }: { categories: Category[] }) => {
+  const { categoryFilters, sort, setCategoryFilters, setSort } = useFilter()
+
+  const handleCategories = (categoryId: string) => {}
+
+  return (
+    <div className={classes.filters}>
+      <div>
+        <h6 className={classes.title}>Product Categories</h6>
+        <div className={classes.categories}>
+          {categories.map(category => {
+            const isSelected = categoryFilters.includes(category.id)
+
+            return (
+              <Checkbox
+                key={category.id}
+                label={category.title}
+                value={category.id}
+                isSelected={isSelected}
+                onClickHandler={handleCategories}
+              />
+            )
+          })}
+        </div>
+        <HR className={classes.hr} />
+        <h6 className={classes.title}>Sort By</h6>
+        <div className={classes.categories}>
+          <RadioButton />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Filters
